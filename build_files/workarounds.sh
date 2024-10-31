ba4b137fcc34e5d2acfd55595667de5f3b9be90a
Tulip Blossom <tulilirockz@outlook.com>
2024-10-30 22:41:10 -0300

    feat: add workaround to make podman-machine work by default OOB

--- build_files/workarounds.sh
+++ build_files/workarounds.sh
@@ -4,3 +4,6 @@ set -oeux pipefail
 
 # alternatives cannot create symlinks on its own during a container build
 ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
+
+# Required by podman machine to work but it is on libexec by default
+ln -sf /usr/bin/virtiofsd /usr/libexec/virtiofsd
